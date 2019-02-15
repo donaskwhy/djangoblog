9e41e8d0fb322a4c848e1c60671deb1622a3dc70
donaskwhy <donaskwhy@nate.com>
2019-02-15 13:01:32 +0900

    login logout complete

--- second/accounts/views.py
+++ second/accounts/views.py
@@ -21,4 +21,20 @@ def signup(request):
         return render(request, 'signup.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    return render(request, 'signup.html')
